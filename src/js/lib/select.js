5bf4450137157002bdf2299d257c090590252771
Ramazan Bakshysh <family@MacBook-Air-Family.local>
2022-11-05 20:18:56 +0300

    finished x2

--- src/js/lib/select.js
+++ src/js/lib/select.js
@@ -1,9 +1,24 @@
 function Select() {
     const select = document.querySelector('.header__wrap-lang-item');
-    const selectContent = document.querySelector('.header__wrap-lang-childs');
+    const selected = document.querySelector('.selected')
+    const menu = document.querySelector('.header__wrap-lang-childs');
+    const option = document.querySelectorAll('.option')
 
     select.addEventListener('click', () => {
-        selectContent.classList.toggle('active');
+        select.classList.toggle('active');
+        menu.classList.toggle('active');
+    });
+
+    option.forEach(o => {
+        o.addEventListener('click', () => {
+            selected.innerHTML = o.innerHTML;
+            select.classList.remove('active');
+            menu.classList.remove('active');
+            option.forEach(o => {
+                o.classList.remove('active')
+            });
+            option.classList.add('active');
+        });
     });
 };
 
